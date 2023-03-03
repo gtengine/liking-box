21a28f4041e0d25ff3673a19e886290527303dd5
gtengine <prierkt@gmail.com>
2023-03-04 00:08:33 +0900

    relocate files to add compose file

--- video-streaming/server.js
+++ video-streaming/server.js
@@ -3,17 +3,18 @@ const fs = require("fs");
 
 const app = express();
 
-// if (!process.env.PORT) {
-//   throw new Error(
-//     "Please specify the port number for the HTTP server with the environment variable PORT."
-//   );
-// }
+if (!process.env.PORT) {
+  throw new Error(
+    "Please specify the port number for the HTTP server with the environment variable PORT."
+  );
+}
 
-// const PORT = process.env.PORT;
-const PORT = 3000;
+const PORT = process.env.PORT;
+// const PORT = 3000;
 
 app.get("/video", (req, res) => {
-  const path = "./karina.mp4";
+  const path = "./karina-blackmamba.mp4";
+  // const path = "./haerin-omg.mp4";
   fs.stat(path, (err, stats) => {
     if (err) {
       console.error("An error occured");
