ee6b7b848321527330ce595ecde2e043f5207257
gtengine <prierkt@gmail.com>
2023-03-06 19:59:35 +0900

    develop containers

---
+++ video-streaming/src/index.js
@@ -0,0 +1,159 @@
+// const express = require("express");
+// const http = require("http");
+// const mongodb = require("mongodb");
+
+// const app = express();
+
+// const PORT = process.env.PORT;
+// const VIDEO_STORAGE_HOST = process.env.VIDEO_STORAGE_HOST;
+// const VIDEO_STORAGE_PORT = parseInt(process.env.VIDEO_STORAGE_PORT);
+// const DB_HOST = process.env.DB_HOST;
+// const DB_NAME = process.env.DB_NAME;
+
+// function main() {
+//   return mongodb.MongoClient.connect(DB_HOST).then((client) => {
+//     const db = client.db(DB_NAME);
+//     const videosCollection = db.collection("videos");
+
+//     app.get("/video", (req, res) => {
+//       const videoId = new mongodb.ObjectId(req.query.id);
+//       videosCollection
+//         .findOne({ _id: videoId })
+//         .then((videoRecord) => {
+//           if (!videoRecord) {
+//             res.sendStatus(404);
+//             return;
+//           }
+//           const forwardRequest = http.request(
+//             {
+//               host: VIDEO_STORAGE_HOST,
+//               port: VIDEO_STORAGE_PORT,
+//               path: `/video?path=${videoRecord.videoPath}`,
+//               method: "GET",
+//               headers: req.headers,
+//             },
+//             (forwardResponse) => {
+//               res.writeHeader(
+//                 forwardResponse.statusCode,
+//                 forwardResponse.headers
+//               );
+//               forwardResponse.pipe(res);
+//             }
+//           );
+//           req.pipe(forwardRequest);
+//         })
+//         .catch((err) => {
+//           console.error("Database query failed.");
+//           console.error((err && err.stack) || err);
+//           res.sendStatus(500);
+//         });
+//     });
+//     app.listen(PORT, () => {
+//       console.log("Microservice Online");
+//     });
+//   });
+// }
+
+// main()
+//   .then(() => console.log("Microservice online."))
+//   .catch((err) => {
+//     console.error("Microservice failed to start.");
+//     console.error((err && err.stack) || err);
+//   });
+
+const express = require("express");
+const fs = require("fs");
+const http = require("http");
+
+//
+// Send the "viewed" to the history microservice.
+//
+function sendViewedMessage(videoPath) {
+  const postOptions = {
+    // Options to the HTTP POST request.
+    method: "POST", // Sets the request method as POST.
+    headers: {
+      "Content-Type": "application/json", // Sets the content type for the request's body.
+    },
+  };
+
+  const requestBody = {
+    // Body of the HTTP POST request.
+    videoPath: videoPath,
+  };
+
+  const req = http.request(
+    // Send the "viewed" message to the history microservice.
+    "http://history/viewed",
+    postOptions
+  );
+
+  req.on("close", () => {
+    console.log("Sent 'viewed' message to history microservice.");
+  });
+
+  req.on("error", (err) => {
+    console.error("Failed to send 'viewed' message!");
+    console.error((err && err.stack) || err);
+  });
+
+  req.write(JSON.stringify(requestBody)); // Write the body to the request.
+  req.end(); // End the request.
+}
+
+//
+// Setup event handlers.
+//
+function setupHandlers(app) {
+  app.get("/video", (req, res) => {
+    // Route for streaming video.
+
+    const videoPath = "./videos/karina-blackmamba.mp4";
+    fs.stat(videoPath, (err, stats) => {
+      if (err) {
+        console.error("An error occurred ");
+        res.sendStatus(500);
+        return;
+      }
+
+      res.writeHead(200, {
+        "Content-Length": stats.size,
+        "Content-Type": "video/mp4",
+      });
+
+      fs.createReadStream(videoPath).pipe(res);
+
+      sendViewedMessage(videoPath); // Send message to "history" microservice that this video has been "viewed".
+    });
+  });
+}
+
+//
+// Start the HTTP server.
+//
+function startHttpServer() {
+  return new Promise((resolve) => {
+    // Wrap in a promise so we can be notified when the server has started.
+    const app = express();
+    setupHandlers(app);
+
+    const port = (process.env.PORT && parseInt(process.env.PORT)) || 3000;
+    app.listen(port, () => {
+      resolve();
+    });
+  });
+}
+
+//
+// Application entry point.
+//
+function main() {
+  return startHttpServer();
+}
+
+main()
+  .then(() => console.log("Microservice online."))
+  .catch((err) => {
+    console.error("Microservice failed to start.");
+    console.error((err && err.stack) || err);
+  });
